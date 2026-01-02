0e311ad409de3df100e91dcbb28332cbda7e502b
prasivo <itsprasoon2004@gmail.com>
2026-01-02 22:01:00 +0530

    Add master syllabus data structure

---
+++ js/data.js
@@ -0,0 +1,14 @@
+const masterSyllabus = {
+    dPharm: {
+        subjects: ["Pharmaceutics", "Pharmaceutical Chemistry", "Pharmacognosy", "Human Anatomy & Physiology", "Social Pharmacy"], //
+        duration: "2 Years"
+    },
+    bPharm: {
+        subjects: ["Remedial Biology/Maths", "Analysis", "Pharmaceutics", "Inorganic Chemistry"], //
+        semesters: 8
+    },
+    gpat: {
+        categories: ["Pharmaceutics", "Pharmacology", "Chemistry", "Pharmacognosy", "Other Subjects"], //
+        totalMarks: 500
+    }
+};
